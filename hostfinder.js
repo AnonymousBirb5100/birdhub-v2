c09059cdae1ccae9134bbaa04059801ad44aa701
AnonymousBirb5100 <31hzeng@stratfordschools.net>
2025-09-23 19:17:15 -0700

    Create hostfinder.js

---
+++ hostfinder.js
@@ -0,0 +1,14 @@
+//Hey, no editing this!
+(function() {
+  const officialHosts = "anonymousbirb5100.github.io/birdhub-v2";
+  //["anonymousbirb5100.github.io","birdhub.netlify.app","playbirdhub.com]; -> store as array for multiple URLs
+  const detectionPopupPage = "detection-popup.html";
+
+  const isEmbedded = window.self !== window.top; //Embed detection
+
+  const isOfficialHost = officialHosts.includes(window.location.hostname); //Domain matching
+
+  if (isEmbedded || !isOfficialHost) { //I said no editing this, please!
+    window.location.href = detectionPopupPage;
+  }
+})();
