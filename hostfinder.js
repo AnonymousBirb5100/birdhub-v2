b38d047b03034c8b30e7e87db0c85823d0186044
AnonymousBirb5100 <31hzeng@stratfordschools.net>
2025-10-09 21:56:24 -0700

    Update hostfinder.js

--- hostfinder.js
+++ hostfinder.js
@@ -1,17 +1,16 @@
 //Hey, no editing this!
 (function() {
-  const officialHosts = ["anonymousbirb5100.github.io",
-                        "birdhubgames.vercel.app"];
-  //["anonymousbirb5100.github.io","birdhub.netlify.app","playbirdhub.com]; -> store as array for multiple URLs
+  const officialHosts = ["anonymousbirb5100.github.io", "birdhubgames.vercel.app"];
   const detectionPopupPage = "detection-popup.html";
 
-  const isEmbedded = window.self !== window.top; //Embed detection
-
-  const isOfficialHost = officialHosts.includes(window.location.hostname); //Domain matching
+  const isEmbedded = window.self !== window.top; // Embed and iframe detection
+  const isGithubHost = window.location.hostname === "anonymousbirb5100.github.io"; // For anonymousbirb5100.github.io/birdhub-v2
+  const isGithubPathValid = window.location.pathname.startsWith("/birdhub-v2"); // (Temporary)
+  const isOfficialHost = officialHosts.includes(window.location.hostname); // Domain match
 
   if (isEmbedded) {
     window.location.href = detectionPopupPage;
-  } else if (!isOfficialHost || !((window.location.hostname === "anonymousbirb5100.github.io" && window.location.pathname.startsWith("/birdhub-v2")))) { //I said no editing this, please!
-    window.location.href = detectionPopupPage;
+  } else if (!isOfficialHost || (isGithubHost && !isGithubPathValid)) { // For Github Pages
+    window.location.href = detectionPopupPage; // I said no editing this please!
   }
 })();
