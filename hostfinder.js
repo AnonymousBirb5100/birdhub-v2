c1fc5d2a57dad2505e96d618e243a2d741cd390b
AnonymousBirb5100 <31hzeng@stratfordschools.net>
2025-10-09 21:19:47 -0700

    Update hostfinder.js

--- hostfinder.js
+++ hostfinder.js
@@ -1,6 +1,6 @@
 //Hey, no editing this!
 (function() {
-  const officialHosts = ["anonymousbirb5100.github.io/birdhub-v2",
+  const officialHosts = ["anonymousbirb5100.github.io",
                         "birdhubgames.vercel.app"];
   //["anonymousbirb5100.github.io","birdhub.netlify.app","playbirdhub.com]; -> store as array for multiple URLs
   const detectionPopupPage = "detection-popup.html";
